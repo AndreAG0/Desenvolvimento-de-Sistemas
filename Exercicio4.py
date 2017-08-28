4c8014b28061ff771771db1bc8d04f1d45726026
AndreAG0 <31414936+AndreAG0@users.noreply.github.com>
2017-08-28 19:10:13 -0300

    Update Exercicio4.py

--- Exercicio4.py
+++ Exercicio4.py
@@ -5,6 +5,11 @@ arquivo.write('Qualquer Coisa')
 arquivo.close()
 arquivo = open("arquivo1.txt", "r")
 
+copia = arquivo.read()
+arquivo.close()
+
 arquivo2 = open("arquivo2.txt", "w")
 
-arquivo2.write(arquivo)
+arquivo2.write(copia)
+
+arquivo2.close()
